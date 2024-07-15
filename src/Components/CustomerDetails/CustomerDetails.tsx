d3d806306bec53e94b817098d8543772bc6e0b16
Div-AHM1ED <ahmedabdelaleem100@gmail.com>
2024-07-15 22:56:47 +0300

    set transctions details

--- src/Components/CustomerDetails/CustomerDetails.tsx
+++ src/Components/CustomerDetails/CustomerDetails.tsx
@@ -22,7 +22,7 @@ function TransactionDetails() {
         <div className="w-1/2 bg-green-200 h-fit rounded-2xl overflow-scroll overflow-x-hidden ">
             <div>{customer?.name}</div>
             <ul className="space-y-6">
-                {customerTransactions.map((trans) => {
+                {customerTransactions.map((trans: ITransactions) => {
                     return <li>{trans.amount}</li>
                 })}
 
